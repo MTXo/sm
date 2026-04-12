0f17714a2bb43d0ff0ee97eb48e2d09843e5508b
drenewoo <drenewoo@pieware.pl>
2026-04-12 22:13:36 +0200

    Dodawanie aplikacji

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -7,7 +7,12 @@ namespace MauiApp1
         public MainPage()
         {
             InitializeComponent();
+            AppCollectionView.ItemsSource = AppScript.apps;
         }
 
+        private void AddApp_Clicked(object sender, EventArgs e)
+        {
+            AppScript.apps.Add(new Scripts.AppInfo { Name = "New App", ExePath = "", SteamAppId = 0 });
+        }
     }
 }
--- MauiApp1/Scripts/AppScript.cs
+++ MauiApp1/Scripts/AppScript.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace MauiApp1.Scripts
 {
+    public struct AppInfo
+    {
+        public string Name { get; set; }
+        public string ExePath { get; set; }
+        public int SteamAppId { get; set; }
+    }
     public static class AppScript
     {
         public static int page = 1;
+
+        public static ObservableCollection<AppInfo> apps = new ObservableCollection<AppInfo>();
+
+
     }
 }
