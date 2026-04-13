b3ed1829669880cbaf3380eb185522d844874a16
drenewoo <drenewoo@pieware.pl>
2026-04-13 23:17:33 +0200

    Dodanie steam app id

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -5,6 +5,7 @@ namespace MauiApp1
     public partial class MainPage : ContentPage
     {
         Scripts.AppInfo current = new Scripts.AppInfo();
+        bool _popupOpen = false;
         public MainPage()
         {
             InitializeComponent();
@@ -17,10 +18,18 @@ namespace MauiApp1
         }
         async void OpenPopup_Clicked(object sender, EventArgs e)
         {
+           
+
+            steamAID.Text = "";
+            exePathEntry.Text = "";
+
             ProcessPicker.Title = "Wybierz proces... ( Ładowanie listy procesów )";
             _ = LoadProcessesAsync();
+
             name_settings.Text = current.Name;
             exePathEntry.Text = current.ExePath;
+            steamAID.Text = current.SteamAppId > 0 ? current.SteamAppId.ToString() : "";
+
             PopupOverlay.IsVisible = true;
 
             // stan początkowy
@@ -30,37 +39,47 @@ namespace MauiApp1
             PopupContent.Opacity = 0;
             PopupContent.TranslationY = 50;
 
-            // animacje równoległe
             await Task.WhenAll(
                 PopupOverlay.FadeToAsync(1, 200),
-
                 PopupContent.FadeToAsync(1, 200),
                 PopupContent.ScaleToAsync(1, 250, Easing.CubicOut),
                 PopupContent.TranslateToAsync(0, 0, 250, Easing.CubicOut)
             );
+            _popupOpen = true;
         }
+
         async Task LoadProcessesAsync()
         {
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                ProcessPicker.ItemsSource = null;
+                ProcessPicker.SelectedIndex = -1;
+                ProcessPicker.SelectedItem = null;
+            });
+
             var processes = await Task.Run(() => ProcessHelpers.GetUserProcesses());
 
-            // wracamy na UI thread
             MainThread.BeginInvokeOnMainThread(() =>
             {
                 ProcessPicker.ItemsSource = processes;
                 ProcessPicker.Title = "Wybierz proces...";
             });
         }
+
         async void ClosePopup_Clicked(object sender, EventArgs e)
         {
+            _popupOpen = false;
+
             await Task.WhenAll(
                 PopupOverlay.FadeToAsync(0, 200),
-
                 PopupContent.FadeToAsync(0, 150),
                 PopupContent.ScaleToAsync(0.8, 150, Easing.CubicIn),
                 PopupContent.TranslateToAsync(0, 50, 150, Easing.CubicIn)
             );
 
             PopupOverlay.IsVisible = false;
+
+            
         }
 
         private void AppCollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -87,13 +106,31 @@ namespace MauiApp1
 
         private void ProcessPicker_SelectedIndexChanged(object sender, EventArgs e)
         {
-            exePathEntry.Text = ProcessHelpers.GetProcessPath(ProcessPicker.SelectedItem?.ToString() ?? "") ?? "";
-            current.ExePath = exePathEntry.Text;
+            try
+            {
+                if (_popupOpen)
+                {
+                    exePathEntry.Text = ProcessHelpers.GetProcessPath(ProcessPicker.SelectedItem?.ToString() ?? "") ?? "";
+                    current.ExePath = exePathEntry.Text;
+                    steamAID.Text = SteamHelper.GetSteamAppIdFromExe(current.ExePath)?.ToString() ?? "0";
+                    current.SteamAppId = int.TryParse(steamAID.Text, out int id) ? id : 0;
+                }
+            } 
+            catch 
+            { 
+                // nic   
+            }
         }
 
         private void exePathEntry_Unfocused(object sender, FocusEventArgs e)
         {
             current.ExePath = exePathEntry.Text;
+            steamAID.Text = SteamHelper.GetSteamAppIdFromExe(current.ExePath)?.ToString() ?? "0";
+            current.SteamAppId = int.TryParse(steamAID.Text, out int id) ? id : 0;
+        }
+        private void steamAIDEntry_Unfocused(object sender, FocusEventArgs e)
+        {
+            current.SteamAppId = int.TryParse(steamAID.Text, out int id) ? id : 0;
         }
     }
 }
